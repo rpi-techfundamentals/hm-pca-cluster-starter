66113f0f79fb9e169a6a7eefdf669cbaccfc43ba
Jason Kuruzovich <jkuruzovich@gmail.com>
2020-03-30 14:48:38 -0400

    updates
    updates

--- starter/tests/q04a.py
+++ starter/tests/q04a.py
@@ -6,7 +6,7 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> round(cluster2_r2_train,2)== 0.59 or round(cluster2_r2_train,2)== 0.49
+          >>> round(cluster2_r2_train,2)== 0.59 or round(cluster2_r2_train,2)== 0.50
           True
           """,
           'hidden': False,
